7c734b1db2668c6fe83c9f3e3c3025886dc0e7b9
Jason Washburn <jason.washburn@gmail.com>
2023-11-11 05:53:22 -0600

    feat: :sparkles: adds basic http server and healthcheck

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "log/slog"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
 
 const version = "1.0.0"
 
@@ -15,5 +22,41 @@ type application struct {
 }
 
 func main() {
+	var cfg config
 
+	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+
+	os.Exit(1)
+}
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
 }

536edac43b77f85613b2c23fca3caf80c1cb0f7f
Jason Washburn <jason.washburn@gmail.com>
2023-11-11 07:06:36 -0600

    refactor: :recycle: adds writeJSON helper and refactor healthcheck to use it

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -54,9 +54,3 @@ func main() {
 
 	os.Exit(1)
 }
-
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
-}

7661a0dd42806b752881aabd48f48f5926d7a23a
Jason Washburn <jason.washburn@gmail.com>
2023-11-15 06:56:14 -0600

    ✨ Add validation to movie creation endpoint

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -14,7 +14,7 @@ func (app *application) logError(r *http.Request, err error) {
 	app.logger.Error(err.Error(), "method", method, "uri", uri)
 }
 
-func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message string) {
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 	env := envelope{"error": message}
 
 	err := app.writeJSON(w, status, env, nil)
@@ -43,4 +43,8 @@ func (app *application) methodNotAllowed(w http.ResponseWriter, r *http.Request)
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }

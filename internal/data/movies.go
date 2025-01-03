8a78632704c68f818ce5c9dcd618a636dba7335f
Jason Washburn <jason.washburn@gmail.com>
2025-01-03 15:19:57 -0600

    feat: Add Movie model and insert logic

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,9 +1,11 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/jasonwashburn/greenlight/internal/validator"
+	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -16,7 +18,7 @@ type Movie struct {
 	Version   int32     `json:"version"`
 }
 
-func ValidateMovie(v *validator.Validator, movie Movie) {
+func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(movie.Title != "", "title", "must be provided")
 	v.Check(len(movie.Title) <= 500, "title", "must be provided")
 	v.Check(movie.Year != 0, "year", "must be provided")
@@ -31,4 +33,31 @@ func ValidateMovie(v *validator.Validator, movie Movie) {
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+  INSERT INTO movies (title, year, runtime, genres)
+  VALUES ($1, $2, $3, $4)
+  RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
